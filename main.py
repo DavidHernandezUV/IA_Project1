fcfef48aaed278a980328b4131c370a67b5f5add
andresfeliper3 <andresfeliper.2001@gmail.com>
2022-10-23 17:09:42 -0500

    cannot get out of the board

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import viewer
 import numpy as np
 
-test = np.loadtxt("Prueba1.txt", dtype='i',delimiter=' ')
-viewer = viewer.Viewer(test);
-viewer.drawState();
+test = np.loadtxt("Prueba1.txt", dtype='i', delimiter=' ')
+viewer = viewer.Viewer(test)
+viewer.drawState()
--- node.py
+++ node.py
@@ -1,7 +1,8 @@
 import numpy as np
 
+
 class Node:
-    #CONSTANTS
+    # CONSTANTS
     LEFT = 0
     DOWN = 1
     RIGHT = 2
@@ -22,41 +23,39 @@ class Node:
         self.ROWS = len(gameBoard)
         self.COLS = len(gameBoard[0])
         self.marioPos = marioPos
-        
 
-    
     def move(self, direction):
 
         sonGameBoard = np.array(self.gameBoard, copy=True)
-        sonMarioPosition = self.marioPos 
-        #MOVE TO LEFT
-        if direction == self.LEFT and (sonGameBoard[self.marioPos[0]][self.marioPos[1]-1] != self.BLOCK):
+        sonMarioPosition = self.marioPos
+        # MOVE TO LEFT
+        if direction == self.LEFT and (sonGameBoard[self.marioPos[0]][self.marioPos[1]-1] != self.BLOCK) and (self.marioPos[1]-1 >= 0):
             sonGameBoard[self.marioPos[0]][self.marioPos[1]-1] = self.MARIO
             sonGameBoard[self.marioPos[0]][self.marioPos[1]] = self.EMPTY
-            #New Mario position
-            sonMarioPosition = (self.marioPos[0],self.marioPos[1]-1)
-        #MOVE TO DOWN
-        if direction == self.DOWN and (sonGameBoard[self.marioPos[0]+1][self.marioPos[1]] != self.BLOCK):
+            # New Mario position
+            sonMarioPosition = (self.marioPos[0], self.marioPos[1]-1)
+        # MOVE TO DOWN
+        if direction == self.DOWN and (sonGameBoard[self.marioPos[0]+1][self.marioPos[1]] != self.BLOCK) and (self.marioPos[0]+1 <= self.ROWS-1):
             sonGameBoard[self.marioPos[0]+1][self.marioPos[1]] = self.MARIO
             sonGameBoard[self.marioPos[0]][self.marioPos[1]] = self.EMPTY
-            #New Mario position
-            sonMarioPosition = (self.marioPos[0]+1,self.marioPos[1])
-        #MOVE TO RIGHT
-        if direction == self.RIGHT and (sonGameBoard[self.marioPos[0]][self.marioPos[1]+1] != self.BLOCK):
+            # New Mario position
+            sonMarioPosition = (self.marioPos[0]+1, self.marioPos[1])
+        # MOVE TO RIGHT
+        if direction == self.RIGHT and (sonGameBoard[self.marioPos[0]][self.marioPos[1]+1] != self.BLOCK) and (self.marioPos[1]+1 <= self.COLS-1):
             sonGameBoard[self.marioPos[0]][self.marioPos[1]+1] = self.MARIO
             sonGameBoard[self.marioPos[0]][self.marioPos[1]] = self.EMPTY
-            #New Mario position
-            sonMarioPosition = (self.marioPos[0],self.marioPos[1]+1)
-        #MOVE TO UP
-        if direction == self.UP and (sonGameBoard[self.marioPos[0]-1][self.marioPos[1]] != self.BLOCK):
+            # New Mario position
+            sonMarioPosition = (self.marioPos[0], self.marioPos[1]+1)
+        # MOVE TO UP
+        if direction == self.UP and (sonGameBoard[self.marioPos[0]-1][self.marioPos[1]] != self.BLOCK) and (self.marioPos[0]-1 >= 0):
             sonGameBoard[self.marioPos[0]-1][self.marioPos[1]] = self.MARIO
             sonGameBoard[self.marioPos[0]][self.marioPos[1]] = self.EMPTY
-            #New Mario position
-            sonMarioPosition = (self.marioPos[0]-1,self.marioPos[1])
-        
+            # New Mario position
+            sonMarioPosition = (self.marioPos[0]-1, self.marioPos[1])
+
+        print(sonGameBoard, sonMarioPosition)
         return sonGameBoard, sonMarioPosition
-        
-            
+
     def showDepth(self):
         print("La profundidad del nodo es:", self.depth)
 
@@ -67,18 +66,18 @@ class Node:
         print(self.gameBoard)
 
 
-gameBoard = np.loadtxt("Prueba1.txt", dtype='i',delimiter=' ')
+gameBoard = np.loadtxt("Prueba1.txt", dtype='i', delimiter=' ')
 
-#Where is Mario
-aux = np.nditer(gameBoard,flags=['multi_index'])
-mariecito = [0,0]
+# Where is Mario
+aux = np.nditer(gameBoard, flags=['multi_index'])
+mariecito = [0, 0]
 for element in aux:
     if element == 2:
         mariecito = aux.multi_index
 print("MARIO ESTÁ EN: ", mariecito)
 
 node = Node(gameBoard, 0, 'izquierda', 2, mariecito)
-node.move(0)
-#node.showDepth()
-#node.showOperator()
-#node.showgameBoard()
+node.move(2)
+# node.showDepth()
+# node.showOperator()
+# node.showgameBoard()
